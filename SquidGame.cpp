3bc1a777f2f91ed19c2f1f82272381288cf2920e
N0zye <84083205+N0zye@users.noreply.github.com>
2023-11-28 16:14:55 +0100

    "Final" commit - added colors and fixed everything

--- SquidGame.cpp
+++ SquidGame.cpp
@@ -1,16 +1,25 @@
-#include <opencv2/opencv.hpp>
+﻿#include <opencv2/opencv.hpp>
 #include <iostream>
 
 class Squid {
 private:
-    void printColor(int red, int green, int blue) {
-        std::cout << "\033[38;2;" << red << ";" << green << ";" << blue << "m*"; //Color print
-        std::cout << "\033[0m"; // Restore to default console after print
+    void printColor(int blue, int green, int red, int alpha) {
+        // Calcola i nuovi valori di RGB basati sul canale alpha
+        red = (red * alpha) / 255;
+        green = (green * alpha) / 255;
+        blue = (blue * alpha) / 255;
+
+        // Stampa il colore con il nuovo valore di alpha
+        std::cout << "\033[48;2;" << red << ";" << green << ";" << blue << "m  ";
+
+        // Ripristina la console al colore predefinito dopo la stampa
+        std::cout << "\033[0m";
     }
+
 public:
     void draw(std::string shape) {
         std::string imagePath = "assets/" + shape + ".png";
-        cv::Mat image = cv::imread(imagePath, cv::IMREAD_GRAYSCALE);
+        cv::Mat image = cv::imread(imagePath, cv::IMREAD_UNCHANGED);
         for (int row = 0; row < image.rows; ++row) {
             for (int col = 0; col < image.cols; ++col) {
                 cv::Vec4b pixel = image.at<cv::Vec4b>(row, col);
@@ -18,7 +27,7 @@ public:
                     std::cout << ' ';                
                 }
                 else {
-                    printColor(pixel[2], pixel[1], pixel[0]);
+                    printColor(pixel[0], pixel[1], pixel[2], pixel[3]);
                 }
             }
             std::cout << std::endl;
@@ -29,9 +38,6 @@ public:
 int main()
 {
     Squid game;
-
     game.draw("cerchio");
-    
-
     return 0;
 }
